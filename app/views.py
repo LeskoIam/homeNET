0a89e0f0a588eee34e24d63b2286268261bf2dc8
Lesko <matevzlesko@gmail.com>
2015-08-23 14:32:36 +0200

    - Added information to aggregator page and added - network summary api (json) "view"

--- app/views.py
+++ app/views.py
@@ -469,6 +469,32 @@ def get_server_data(back_period="None"):
     return jsonify(**data)
 
 
+@app.route("/api/network_summary")
+def get_network_summary():
+    # Out of M nodes (n_all_nodes) N are active (pinged) and U nodes are up
+    n_all_nodes = db.session.query(db.func.count(Nodes.id)).one()[0]
+    n_all_active_nodes = db.session.query(db.func.count(Nodes.id)).filter(Nodes.in_use == True).one()[0]
+    last_common_id = db.session.query(LastEntry.last_common_id).filter(LastEntry.ready_to_read == True). \
+        order_by(LastEntry.date_time.desc()).first()[0]
+    n_all_up_nodes = db.session.query(db.func.count(PingerData.id)).filter(Nodes.id == PingerData.node_id). \
+        filter(PingerData.common_id == last_common_id). \
+        filter(PingerData.up == True).one()[0]
+
+    print n_all_nodes, type(n_all_nodes)
+    print n_all_active_nodes, type(n_all_active_nodes)
+    print last_common_id
+    print n_all_up_nodes
+
+    data = {
+        "network": {
+            "n_all_nodes": n_all_nodes,
+            "n_all_active_nodes": n_all_active_nodes,
+            "n_all_up_nodes": n_all_up_nodes
+        }
+    }
+    return jsonify(data)
+
+
 # ###################################################
 #                                                   #
 #             HELPER FUNCTIONS & CLASSES            #
